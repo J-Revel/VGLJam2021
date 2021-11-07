022ab2eb6c6c3a322bc9c8a8ee1095e188251639
MichaelAssing <72279741+MichaelAssing@users.noreply.github.com>
2021-11-07 22:50:20 +0100

    curseurs

--- VGLJam2021/Assets/Graph/UI/CursorManager.cs
+++ VGLJam2021/Assets/Graph/UI/CursorManager.cs
@@ -4,10 +4,15 @@ using System.Collections.Generic;
 
 public class CursorManager : MonoBehaviour
 {
+    public float vectorA = 0;
+    public float vectorB = 0;
     public Texture2D cursorTex;
-    Vector2 hotSpot = new Vector2(50, 0);
+    //Vector2 hotSpot = new Vector2(vectorA, vectorB);
    
-    void Awake() { Cursor.SetCursor(cursorTex, hotSpot, CursorMode.ForceSoftware); }
+    void Awake() {
+
+        Vector2 hotSpot = new Vector2(vectorA, vectorB);
+        Cursor.SetCursor(cursorTex, hotSpot, CursorMode.ForceSoftware); }
 
   
 }
